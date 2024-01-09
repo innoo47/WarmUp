024d1e055e509831b978b7ef5ed5aaf511a32326
박인호 <i0n4h0o7@gmail.com>
2024-01-10 03:41:52 +0900

    Initial Commit

---
+++ WarmUp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WarmUp
+//
+//  Created by 박인호 on 1/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WarmUp/WarmUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarmUpApp.swift
+//  WarmUp
+//
+//  Created by 박인호 on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WarmUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
