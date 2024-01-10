9bfae2d3702a52d3e963a794fcae7376a605a016
박인호 <i0n4h0o7@gmail.com>
2024-01-11 06:38:54 +0900

    1. 과일 리스트 추가

---
+++ WarmUp/ListLoop.swift
@@ -0,0 +1,18 @@
+//
+//  ListLoop.swift
+//  WarmUp
+//
+//  Created by 박인호 on 1/11/24.
+//
+
+import SwiftUI
+
+struct ListLoop: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListLoop()
+}
