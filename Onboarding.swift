1543c6d0be66b95b048dab97f3a467bcd0f8f211
박인호 <i0n4h0o7@gmail.com>
2024-01-11 23:30:40 +0900

    2. 온보드 화면 생성

---
+++ Onboarding.swift
@@ -0,0 +1,18 @@
+//
+//  Onboarding.swift
+//  WarmUp
+//
+//  Created by 박인호 on 1/11/24.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Onboarding()
+}
